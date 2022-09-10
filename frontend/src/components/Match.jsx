ef9b00391365980703b4535769773af8f28eaf56
Shasta Johnsen-Sollos <johnsens@ualberta.ca>
2022-09-10 21:09:37 +0000

    Add Match card component

---
+++ frontend/src/components/Match.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Card, CardContent, Typography } from '@mui/material';
+import PropTypes from 'prop-types';
+
+export class MatchInfo {
+  constructor(uid, name, percentage, contactInfo, contactType) {
+    this.uid = uid;
+    this.name = name;
+    this.percentage = percentage;
+    this.contactInfo = contactInfo;
+    this.contactType = contactType;
+  }
+}
+
+export function Match(props) {
+  const { match } = props;
+  return (
+    <Card elevation={2} sx={{ m: 2 }}>
+      <CardContent>
+        <Typography variant="h5" sx={{ fontWeight: 'bold' }}>{match.name}</Typography>
+        <Typography variant="h4" sx={{ fontWeight: 'bold', mb: 2 }}>{`${match.percentage}% Match` }</Typography>
+        <Typography variant="subtitle1">{`${match.contactType.toUpperCase()}: ${match.contactInfo}`}</Typography>
+      </CardContent>
+    </Card>
+  );
+}
+
+Match.propTypes = {
+  match: PropTypes.instanceOf(MatchInfo).isRequired,
+};
