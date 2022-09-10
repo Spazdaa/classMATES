e1b8a02f864d63a03b9806945a6778b78febb59c
Shasta Johnsen-Sollos <johnsens@ualberta.ca>
2022-09-10 22:53:04 +0000

    Modify component for uploading ics files

--- frontend/src/components/UploadIcs.jsx
+++ frontend/src/components/UploadIcs.jsx
@@ -1,10 +1,30 @@
 import { Button } from '@mui/material';
-import React from 'react';
+import React, { useEffect } from 'react';
+import api from '../api/api';
 
 export default function UploadIcs() {
+  const [filename, setFilename] = React.useState('No file chosen');
+
+  useEffect(() => {
+    // eslint-disable-next-line no-console
+    console.log(filename);
+    const reader = new FileReader();
+    if (filename !== 'No file chosen') {
+      reader.readAsText(filename);
+      reader.onload = async (e) => {
+        // eslint-disable-next-line no-console
+        console.log(e.target.result);
+        await api.uploadIcs();
+      };
+    }
+  }, [filename]);
+
   return (
-    <Button variant="contained">
-      Click  me
+    <Button variant="contained" component="label" sx={{ height: 100, mt: 4, backgroundColor: 'green' }}>
+      {'Add your schedule >///<'}
+      <span>
+        <input hidden accept=".ics" type="file" onChange={(e) => { setFilename(e.target.files[0]); }} />
+      </span>
     </Button>
   );
 }
