dfd5522f3e8c1db81b067f450db9cc0e9e706c74
Steven <flyrobot27@gmail.com>
2022-09-10 15:08:36 -0600

    refactored database. Previous class objects simplified to a single Course model

--- backend/classmate_backend/api/models.py
+++ backend/classmate_backend/api/models.py
@@ -11,27 +11,8 @@ class Contact(models.Model):
     contact_type = models.CharField(max_length=200)
     models.UniqueConstraint(fields=['uid', 'contact_id'], name='unique_contact')
 
-class ClassSubject(models.Model):
-    subject_id = models.UUIDField(primary_key=True)
-    subject = models.CharField(max_length=10,unique=True)
-
-class ClassNumber(models.Model):
-    subject_id=models.ForeignKey(ClassSubject, on_delete=models.CASCADE)
-    number_id = models.UUIDField(primary_key=True)
-    models.UniqueConstraint(fields=['subject_id', 'number_id'], name='unique_number')
-    number = models.CharField(max_length=5)
-
-class ClassSection(models.Model):
-    subject_id=models.ForeignKey(ClassSubject, on_delete=models.CASCADE)
-    number_id=models.ForeignKey(ClassNumber, on_delete=models.CASCADE)
-    section_id = models.UUIDField(primary_key=True)
-    models.UniqueConstraint(fields=['subject_id', 'number_id','section_id'], name='unique_section')
-    number = models.CharField(max_length=5)
-
-class ClassUsers(models.Model):
-    cuid=models.UUIDField(primary_key=True)
-    uid=models.ForeignKey(AppUsers, on_delete=models.CASCADE)
-    subject_id=models.ForeignKey(ClassSubject, on_delete=models.CASCADE)
-    number_id=models.ForeignKey(ClassNumber, on_delete=models.CASCADE)
-    section_id=models.ForeignKey(ClassSection, on_delete=models.CASCADE)
-    models.UniqueConstraint(fields=['subject_id', 'number_id','section_id','uid','cuid'], name='unique_class_users')
+class Courses(models.Model):
+    id = models.UUIDField(primary_key=True)
+    uid = models.ForeignKey(AppUsers, on_delete=models.CASCADE)
+    section = models.CharField(max_length=20)
+    course = models.CharField(max_length=20)
