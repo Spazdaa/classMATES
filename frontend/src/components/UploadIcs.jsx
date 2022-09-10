f3f0b6052d5d7e31ec5ae7629297926a018bcf44
Shasta Johnsen-Sollos <johnsens@ualberta.ca>
2022-09-10 21:51:19 +0000

    Create component for uploading ICS

---
+++ frontend/src/components/UploadIcs.jsx
@@ -0,0 +1,10 @@
+import { Button } from '@mui/material';
+import React from 'react';
+
+export default function UploadIcs() {
+  return (
+    <Button variant="contained">
+      Click  me
+    </Button>
+  );
+}
