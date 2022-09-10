2f28dadf61399700370b13bb1e1c5e1eb32519db
Alex Nordstrom <6267244+HyperEpsilon@users.noreply.github.com>
2022-09-10 16:22:42 -0600

    Added sessionStorage for auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Dashboard from './components/Dashboard/Dashboard';
 import Login from './components/Login/Login';
 import Preferences from './components/Preferences/Preferences';
+import useToken from './components/App/useToken';
 
 function App() {
-  const [token, setToken] = useState();
+  const { token, setToken } = useToken();
 
   if (!token) {
     return <Login setToken={setToken} />;
