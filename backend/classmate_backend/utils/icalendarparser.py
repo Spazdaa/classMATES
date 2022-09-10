e665930e7ec135380cc2c29260a79fa89fbe397b
Steven Heung <flyrobot27@gmail.com>
2022-09-10 15:14:15 -0600

    Modified calendar parser to reflect changes to api model

--- backend/classmate_backend/utils/icalendarparser.py
+++ backend/classmate_backend/utils/icalendarparser.py
@@ -3,8 +3,7 @@ from icalendar import Calendar
 class Class:
     """
         This class represents a Class in the schedule, it contains its:
-            Subject (e.g. CMPUT, ENG)
-            Number (e.g. 266, 401)
+            Course (e.g. STAT 266, CMPUT 401)
             Section (e.g. LEC A1, SEM F1)
         To create: Pass in the course name summary (e.g. CMPUT 401 LEC A1)
         To access: Call the getters (e.g. get_subject)
@@ -21,23 +20,15 @@ class Class:
     
     def __parse(self) -> None:
         parsed = self.__summary.split(" ", maxsplit=2)
-        self.__subject = parsed[0]
-        self.__number = parsed[1]
+        self.__course = parsed[0] + " " + parsed[1]
         self.__section = parsed[2]
     
-    def get_subject(self) -> str:
-        """
-            Gets the subject of the class
-            Output: the subject (e.g. CMPUT)
-        """
-        return self.__subject
-    
-    def get_number(self) -> str:
+    def get_course(self) -> str:
         """
             Gets the course number
-            Output: The course number (e.g. 266)
+            Output: The course number (e.g. STAT 266)
         """
-        return self.__number
+        return self.__course
     
     def get_section(self) -> str:
         """
@@ -63,7 +54,7 @@ def parseCalendar(ics: str) -> list:
     
     classes = list()
     for s in summaries:
-        c : Class = Class(summary=s)
+        c = Class(summary=s)
         classes.append(c)
 
     return classes
