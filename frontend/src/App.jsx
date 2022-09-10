17804323c5215b80bf783de5a2d10f2d80ab55e9
Shasta Johnsen-Sollos <johnsens@ualberta.ca>
2022-09-10 21:10:26 +0000

    Add MatchList to main app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from './api/api';
+import MatchList from './components/MatchList';
 
 function App() {
+  const [matches, setMatches] = useState([]);
+  useEffect(() => {
+    api.getMatches(1, 5).then((response) => {
+      setMatches(response);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <h1>Hello Jashwanth!</h1>
+      <MatchList matches={matches} />
     </div>
   );
 }
