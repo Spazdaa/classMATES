2eeb8ea3909d861d78a42746a4fe9448f52d2f93
Steven Heung <flyrobot27@gmail.com>
2022-09-10 18:50:59 -0600

    Add CORS module

--- backend/classmate_backend/api/urls.py
+++ backend/classmate_backend/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import re_path
 
-from api.api import LoginAPI, RegisterAPI
+from api.api import LoginAPI, RegisterAPI, ClassViewSet
 
 urlpatterns = [
     re_path(r"register/?$", RegisterAPI.as_view()),
     re_path(r"login/?$", LoginAPI.as_view()),
+    #re_path(r"upload/?$", ClassViewSet.as_view())
 ]
