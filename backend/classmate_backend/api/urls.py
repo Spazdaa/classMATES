47dc85cdd5ccab43475925166d7f26132f27182a
Steven Heung <flyrobot27@gmail.com>
2022-09-10 19:28:47 -0600

    Changed to APIView for Calendar

--- backend/classmate_backend/api/urls.py
+++ backend/classmate_backend/api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import re_path
 
-from api.api import LoginAPI, RegisterAPI, CalendarViewSet
+from api.api import LoginAPI, RegisterAPI, CalendarAPI
 
 urlpatterns = [
     re_path(r"register/?$", RegisterAPI.as_view()),
     re_path(r"login/?$", LoginAPI.as_view()),
-    re_path(r"upload/?$", CalendarViewSet.as_view({"put": "put"}))
+    re_path(r"upload/?$", CalendarAPI.as_view())
 ]
