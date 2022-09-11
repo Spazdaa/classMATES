e47c993fbb94ba4ab4e0bb0dc0268b0d50bc2a20
Steven Heung <flyrobot27@gmail.com>
2022-09-11 14:08:18 -0600

    Add default uuid field for AppUser

--- backend/classmate_backend/api/models.py
+++ backend/classmate_backend/api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class AppUsers(AbstractUser):
-    uid = models.UUIDField(primary_key=True)
+    uid = models.UUIDField(primary_key=True, default=uuid.uuid4)
 
 class Contact(models.Model):
     uid = models.ForeignKey(AppUsers, on_delete=models.CASCADE)
