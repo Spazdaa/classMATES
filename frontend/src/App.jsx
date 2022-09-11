a17b584ac26e6b54c163c54990611d5b2c92f86c
Alex Nordstrom <6267244+HyperEpsilon@users.noreply.github.com>
2022-09-10 22:32:08 -0600

    Integrated the login/signup with the existing app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import './App.css';
+
 import Dashboard from './components/Dashboard/Dashboard';
+import Logout from './components/Logout/Logout';
+import LoginRegisterTabbed from './components/LoginRegisterTabbed/LoginRegisterTabbed';
+import useToken from './components/App/useToken';
 
 function App() {
+  const { token, setToken } = useToken();
+
+  if (!token) {
+    return (
+      <LoginRegisterTabbed setToken={setToken} />
+    );
+  }
+
   return (
-    <Dashboard />
+    <div className="wrapper">
+      <div>
+        <Logout />
+      </div>
+      <div>
+        <Dashboard />
+      </div>
+    </div>
   );
 }
 
